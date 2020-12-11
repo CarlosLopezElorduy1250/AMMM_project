ecddee0d75b6807d7791b44d16cc1dc2cc3d8f6d
Retrone1250 <carlos.lopez.elorduy2@gmail.com>
2020-12-11 18:27:09 +0100

    Small changes

--- ALGORITHMS/greedy.py
+++ ALGORITHMS/greedy.py
@@ -20,19 +20,6 @@ location2sumdist = {}   # Sum of distances for each location to each city
 centre2cities_primary   = {}    # Dictionary relating each centre with the cities where it serves as a primary centre       -> centre2cities_primary[Centre]   = [city1, city2, ...]
 centre2cities_secondary = {}    # Dictionary relating each centre with the cities where it serves as a secondary centre     -> centre2cities_secondary[Centre] = [city1, city2, ...]
 
-# city2centres            = {}    # Dictionary that relates each city with primary and secondary centre                       -> city2centres[City] = (primary, secondary)
-
-# city2isComplete         = {}    # Dictionary relating each city to a boolean. True -> has both primary and secondary | False -> otherwise
-# for c in name2city.keys():
-#     city2isComplete[c] = False
-
-# def add_cType2location(c_type, location):
-#     '''
-#     Adds the type of center to the location
-#     '''
-
-#     global name2location
-#     name2location[location].type = c_type
 
 
 def sort_type_cost(name2type = name2type):
@@ -127,8 +114,6 @@ if __name__ == "__main__":
                     if name2city[c].primary_l is not None and name2city[c].secondary_l is not None:
                         unserved_c.remove(c)
 
-
-
             f0 = False
             new_location = selectLocation(unoccupied_l, unserved_c)
 
@@ -143,42 +128,3 @@ if __name__ == "__main__":
                     min_location = new_location
 
 
-
-        
-
-
-
-    # index_minCost = 0     # Index that tells us which type are we currently trying to introduce in locations
-    # cost_order = sort_type_cost()
-
-    # while len(unserved_c)>0:
-    #     current_location = random.sample(unoccupied_l, 1)[0]
-    #     add_cType2location(cost_order[index_minCost], current_location)
-
-    #     for c in name2city.keys():
-    #         if c in unserved_c:
-    #             dist_c2l = distance( (name2city[c].cx, name2city[c].cy) , (name2location[current_location].lx, name2location[current_location].ly) )
-                
-    #             if  dist_c2l < d_city[index_minCost]:
-    #                 if name2city[c].primary_l is not None and name2city[c].secondary_l is None:
-    #                     name2city[c].secondary_l = current_location
-    #                 elif name2city[c].primary_l is None:
-    #                     name2city[c].primary_l = current_location
-                
-    #             elif dist_c2l < 3*d_city[index_minCost]:
-    #                 if name2city[c].secondary_l is None:
-    #                     name2city[c].secondary_l = current_location
-            
-    #             if name2city[c].primary_l is not None and name2city[c].secondary_l is not None:
-    #                 unserved_c.remove(c)
-    #     unoccupied_l.remove(current_location)
-
-    
-
-# location2cities_primary   = {}
-# location2cities_secondary = {}
-
-# for location in name2location.keys():
-#     location2cities_primary[location] = []
-#     for city in name2city.keys():
-#         if distance( (name2location[location].lx, name2location[location].ly), (name2city[city].cx, name2city[city].cy)) < 
