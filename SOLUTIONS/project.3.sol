b4f8701574ce1be68145d656597a81bdc67442b7
Paula Iborra de Toledo <>
2020-12-13 21:00:38 +0100

    final changes. Fixed filled cap.

--- SOLUTIONS/project.3.sol
+++ SOLUTIONS/project.3.sol
@@ -1,19 +1,19 @@
 Data from: DATA/project.3.dat
-Execution time: 0.00057
+Execution time: 0.13621
 ##################################################################################################
 
 Solution with cost: 72 
 
-City (3, 2) is served by: Primary center in location (1, 2) || Secondary center in location (3, 1) 
-City (1, 4) is served by: Primary center in location (3, 1) || Secondary center in location (1, 2) 
-City (1, 3) is served by: Primary center in location (1, 2) || Secondary center in location (3, 1) 
-City (1, 2) is served by: Primary center in location (1, 2) || Secondary center in location (3, 1) 
-City (4, 3) is served by: Primary center in location (3, 1) || Secondary center in location (1, 2) 
-City (2, 2) is served by: Primary center in location (1, 2) || Secondary center in location (3, 1) 
-City (0, 1) is served by: Primary center in location (1, 2) || Secondary center in location (3, 1) 
+City (3, 2) is served by: Primary center in location (1, 3) || Secondary center in location (3, 1) 
+City (1, 4) is served by: Primary center in location (3, 1) || Secondary center in location (1, 3) 
+City (1, 3) is served by: Primary center in location (1, 3) || Secondary center in location (3, 1) 
+City (1, 2) is served by: Primary center in location (1, 3) || Secondary center in location (3, 1) 
+City (4, 3) is served by: Primary center in location (1, 3) || Secondary center in location (3, 1) 
+City (2, 2) is served by: Primary center in location (3, 1) || Secondary center in location (1, 3) 
+City (0, 1) is served by: Primary center in location (1, 3) || Secondary center in location (3, 1) 
 
-Location (3, 1) has a center of type 2. 
-Location (1, 2) has a center of type 0. 
+Location (3, 1) has a center of type 2. It serves 7.8 inhabitants, max is 8.
+Location (1, 3) has a center of type 0. It serves 18.6 inhabitants, max is 19.
 
 ##################################################################################################
 
